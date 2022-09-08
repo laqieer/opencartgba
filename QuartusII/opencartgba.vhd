d5835dfd49ceef9022b3f2001adabd2a1990d5fa
laqieer <laqieer@126.com>
2022-09-08 22:45:06 +0800

    Fix mapper's HDL design

--- QuartusII/opencartgba.vhd
+++ QuartusII/opencartgba.vhd
@@ -4,27 +4,23 @@ use ieee.numeric_std.all;
 
 entity opencartgba is
 	port (
-		GBA_AD	: in	std_logic_vector(23 downto 0);
+		GBA_AD	: in	std_logic_vector(15 downto 0);
 		GBA_CS	: in	std_logic;
 		GBA_RD	: in	std_logic;
-		GBA_WR	: in	std_logic;
-		ROM_A		: out	std_logic_vector(23 downto 0)
+		ROM_A		: out	std_logic_vector(15 downto 0)
 	);
 end opencartgba;
 
 architecture cart of opencartgba is
-  signal ADDR : std_logic_vector(23 downto 0);
-  signal ADDR_INC : std_logic_vector(23 downto 0);
+	signal ADDR : std_logic_vector(15 downto 0);
+	signal ADDR_INC : std_logic_vector(15 downto 0);
 begin
-	process (GBA_RD, GBA_CS, GBA_WR, GBA_AD) is
+	process (GBA_RD, GBA_CS, GBA_AD) is
 	begin
 		if GBA_CS = '1' then
 			ADDR <= GBA_AD;
-			ADDR_INC <= GBA_AD;
-		elsif GBA_WR = '0' or GBA_RD = '0' then
-			ADDR_INC <= std_logic_vector(unsigned(ADDR) + 1);
-		else
-			ADDR <= ADDR_INC;
+		elsif rising_edge(GBA_RD) then
+			ADDR <= std_logic_vector(unsigned(ADDR) + 1);
 		end if;
 	end process;
 	ROM_A <= ADDR;
